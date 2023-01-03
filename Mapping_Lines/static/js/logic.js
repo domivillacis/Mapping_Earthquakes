41e33c94ea0caca8002fde2cfb34c2ac5284b811
domivillacis <domi.villacism@gmail.com>
2023-01-03 12:36:50 -0500

    adding mapping lines code

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,14 +2,25 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-//  Add a marker to the map for Los Angeles, California.
-// L.circleMarker([34.0522, -118.2437], {
-//     color: 'black',
-//     fillColor: '#ffffa1',
-//     radius: 300
-//  }).addTo(map);
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6214, -122.3790]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+  }).addTo(map);
+
+// //  Add a marker to the map for Los Angeles, California.
+// // L.circleMarker([34.0522, -118.2437], {
+// //     color: 'black',
+// //     fillColor: '#ffffa1',
+// //     radius: 300
+// //  }).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
